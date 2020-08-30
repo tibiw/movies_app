187f70a1271287b47c6975639f3bf821e9834815
Tibor Waxmann <tibor.waxmann@gmail.com>
2020-08-30 22:01:06 +0300

    Data source (2)

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "561ebd3f807bef00aa61656127f7623f";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:movies_app/data/core/api_client.dart';
+import 'package:movies_app/data/data_sources/movie_remote_data_source.dart';
 
 void main() {
+  ApiClient apiClient = ApiClient(Client());
+  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
+  dataSource.getTrending();
+  dataSource.getPopular();
+  dataSource.getPlayingNow();
+  dataSource.getComingSoon();
   runApp(MyApp());
 }
 
