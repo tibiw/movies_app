64406969d709165f6ef5a2e91944b17d736f843f
Tibor Waxmann <tibor.waxmann@gmail.com>
2020-09-09 21:54:12 +0300

    Dependency injection (4)

---
+++ lib/di/get_it.dart
@@ -0,0 +1,37 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:movies_app/data/core/api_client.dart';
+import 'package:movies_app/data/data_sources/movie_remote_data_source.dart';
+import 'package:movies_app/data/repositories/movie_repository_impl.dart';
+import 'package:movies_app/domain/repositories/movie_repository.dart';
+import 'package:movies_app/domain/usecases/get_comming_soon.dart';
+import 'package:movies_app/domain/usecases/get_playing_now.dart';
+import 'package:movies_app/domain/usecases/get_popular.dart';
+import 'package:movies_app/domain/usecases/get_trending.dart';
+
+final getItInstance = GetIt.I;
+
+Future init() async {
+  getItInstance.registerLazySingleton<Client>(() => Client());
+
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetPlayingNow>(
+      () => GetPlayingNow(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetCommingSoon>(
+      () => GetCommingSoon(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRepository>(
+      () => MovieRepositoryImpl(getItInstance()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movies_app/data/core/api_client.dart';
-import 'package:movies_app/data/data_sources/movie_remote_data_source.dart';
-import 'package:movies_app/data/repositories/movie_repository_impl.dart';
+import 'package:movies_app/domain/entities/no_params.dart';
+
 import 'package:movies_app/domain/entities/app_error.dart';
 import 'package:movies_app/domain/entities/movie_entity.dart';
-import 'package:movies_app/domain/entities/no_params.dart';
-import 'package:movies_app/domain/repositories/movie_repository.dart';
-import 'package:movies_app/domain/usecases/get_comming_soon.dart';
-import 'package:movies_app/domain/usecases/get_playing_now.dart';
-import 'package:movies_app/domain/usecases/get_popular.dart';
+// import 'package:movies_app/domain/usecases/get_comming_soon.dart';
+// import 'package:movies_app/domain/usecases/get_playing_now.dart';
+// import 'package:movies_app/domain/usecases/get_popular.dart';
 import 'package:movies_app/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
 void main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
-  GetPopular getPopular = GetPopular(movieRepository);
-  GetPlayingNow getPlayingNow = GetPlayingNow(movieRepository);
-  GetCommingSoon getCommingSoon = GetCommingSoon(movieRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
+  // GetPopular getPopular = GetPopular(movieRepository);
+  // GetPlayingNow getPlayingNow = GetPlayingNow(movieRepository);
+  // GetCommingSoon getCommingSoon = GetCommingSoon(movieRepository);
   final Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold(
